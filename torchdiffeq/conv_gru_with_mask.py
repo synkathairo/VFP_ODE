2c8d9fdf243221fe032ce05fff3771a77c097552
aneesh-shetye <axs10302@nyu.edu>
2023-12-18 16:58:44 -0500

    corrected indents. Back to single-GPU structure

--- torchdiffeq/conv_gru_with_mask.py
+++ torchdiffeq/conv_gru_with_mask.py
@@ -82,125 +82,101 @@ device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 
 
 ############################################
 
-# SLURM AND MULTIGPU TRAINING: 
-#region: MAIN FUNCTION:
 def main(): 
-
-    # single-node distributed training
-    args.rank = 0
-    args.dist_url = 'tcp://localhost:58472'
-    args.world_size = 1 
-    ############################################
-    args.ngpus_per_node = args.world_size #single machine 
-    ############################################
-    # print(args.ngpus_per_node)
-    # torch.multiprocessing.spawn(main_worker, (args,), args.ngpus_per_node)
-    torch.multiprocessing.spawn(main_worker, args=(args,), nprocs=args.ngpus_per_node)
-#endregion
     
-
-
-def main_worker(gpu, args): 
-    
-	args.rank +=gpu
-
-	torch.distributed.init_process_group(
-	        backend='nccl', init_method=args.dist_url,
-			        world_size=args.world_size, rank=args.rank)
-
-    torch.cuda.set_device(gpu)
-    torch.backends.cudnn.benchmark = True
-
 	args.train = True
 
 	mmnist_train = build(training=True, args=args)
 
+	func = Model(inchannels=3)#.to(args.rank)
 
-    func = Model(inchannels=3).to(args.rank)
-	func = torch.nn.parallel.DistributedDataParallel(func, device_ids = [gpu], find_unused_parameters=True)
+	conv_enc = ConvEncoder()
 
-    conv_enc = ConvEncoder().to(args.rank)
-	conv_enc = torch.nn.parallel.DistributedDataParallel(conv_enc, device_ids = [gpu], find_unused_parameters=True)
-    conv_dec = ConvDecoder(inp_dim=3).to(args.rank)
-	conv_dec = torch.nn.parallel.DistributedDataParallel(conv_dec, device_ids = [gpu], find_unused_parameters=True)
+	conv_dec = ConvDecoder(inp_dim=3)
 
-    optimizer = torch.optim.Adamax(func.parameters(), lr=args.learning_rate)
+	optimizer = torch.optim.Adamax(func.parameters(), lr=args.learning_rate)
 
-    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
-    warmup_scheduler = warmup.LinearWarmup(optimizer, warmup_period=args.warmup_period)
-
-	sampler = torch.utils.data.distributed.DistributedSampler(mmnist_train)
+	lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+	warmup_scheduler = warmup.LinearWarmup(optimizer, warmup_period=args.warmup_period)
 
 	train_loader = torch.utils.data.DataLoader(
 			dataset=mmnist_train, batch_size=args.batch_size, num_workers=args.workers,
-			pin_memory=True, sampler=sampler,  collate_fn = MyCollate(nframes=args.nframes, frame_size=args.frame_size))
+			pin_memory=True,  collate_fn = MyCollate(nframes=args.nframes, frame_size=args.frame_size))
 
-    epochs = args.epochs
+	epochs = args.epochs
 
-    for epoch in range(epochs):  
+	for epoch in range(epochs):  
+
 
 		func.train()
 		conv_enc.train()
 		conv_dec.train()
 
-		sampler.set_epoch(epoch)
 		epoch_loss = 0 
 
-        for n, i in enumerate(train_loader): 
+		for n, i in enumerate(train_loader): 
 
             #i.shape = batch, time, frame_size, frame_size
-            inp = i[:, 0, :, :].cuda(gpu, non_blocking=True) #batch_size, time(=1), frame_size, frame_size
-            t  = torch.linspace(0, 1, i.shape[1]-1)#[:8]
-            trg = i[:, 1:, :, :].cuda(gpu, non_blocking=True) #batch, time, frame_size, frame_size
 
-            optimizer.zero_grad()
+			inp = i[:, 0, :, :]#.cuda(gpu, non_blocking=True) #batch_size, time(=1), frame_size, frame_size
+
+			t  = torch.linspace(0, 1, i.shape[1]-1)#[:8]
+
+			trg = i[:, 1:, :, :]#.cuda(gpu, non_blocking=True) #batch, time, frame_size, frame_size
+
             # pred = odeint(func, inp, t).to(device) #pred.shape = time, batch_size, frame_size, frame_size
-            h_curr = torch.zeros(i[:, 0, :, :].unsqueeze(1).shape).repeat(1,3,1,1).cuda(gpu, non_blocking=True)
-            pred = torch.ones(i.shape)[:, 0, :, :].unsqueeze(1).cuda(gpu, non_blocking=True)
+
+			h_curr = torch.zeros(i[:, 0, :, :].unsqueeze(1).shape).repeat(1,3,1,1)#.cuda(gpu, non_blocking=True)
+			pred = torch.ones(i.shape)[:, 0, :, :].unsqueeze(1)#.cuda(gpu, non_blocking=True)
             # pred = i[:, 0, :, :].unsqueeze(1)
-            img_diff_seq = torch.zeros(i.shape)[:, 0, :, :].unsqueeze(1).cuda(gpu, non_blocking=True)
 
-            for time in range(i.shape[1]-1): 
+			img_diff_seq = torch.zeros(i.shape)[:, 0, :, :].unsqueeze(1)#.cuda(gpu, non_blocking=True)
 
-                inp = i[:,time, :, : ].unsqueeze(1)
-                mean = torch.mean(inp, dim=(-1, -2)).unsqueeze(-1)
-                mean = mean.unsqueeze(-1).repeat(1,1,inp.shape[2],inp.shape[3])
-                inp = inp-mean
+			for time in range(i.shape[1]-1): 
 
-                h_next = odeint(func, h_curr, t).to(device)[-1, :, :, :] #pred.shape = time, batch_size, frame_size, frame_size
-                out, h_next = conv_enc(inp, h_next)
-                of, img_diff, out = conv_dec(h_next, inp)
-                h_curr = h_next 
-                pred = pred + mean
-                pred = torch.cat((pred,out), dim=1) 
-                img_diff_seq = torch.cat((img_diff_seq, img_diff), dim=1)
+				inp = i[:,time, :, : ].unsqueeze(1)
+				mean = torch.mean(inp, dim=(-1, -2)).unsqueeze(-1)
+				mean = mean.unsqueeze(-1).repeat(1,1,inp.shape[2],inp.shape[3])
+				inp = inp-mean
 
-            pred = pred[:, :-1, :, :]
-            img_diff_seq = img_diff_seq[:, 1:, :, :]
+				h_next = odeint(func, h_curr, t).to(device)[-1, :, :, :] #pred.shape = time, batch_size, frame_size, frame_size
+				out, h_next = conv_enc(inp, h_next)
+				of, img_diff, out = conv_dec(h_next, inp)
+				h_curr = h_next 
+				pred = pred + mean
+				pred = torch.cat((pred,out), dim=1) 
+				img_diff_seq = torch.cat((img_diff_seq, img_diff), dim=1)
+
+			pred = pred[:, :-1, :, :]
+			img_diff_seq = img_diff_seq[:, 1:, :, :]
 
 			optimizer.zero_grad()
             #mse_loss: 
-            loss = torch.mean(torch.abs(pred-trg))
-            id_trg = i[:, :-1, :, :] - trg
-            loss_id = torch.mean(torch.abs(img_diff_seq - id_trg)) 
-            loss = loss + loss_id
+
+			loss = torch.mean(torch.abs(pred-trg))
+
+			id_trg = i[:, :-1, :, :] - trg
+			loss_id = torch.mean(torch.abs(img_diff_seq - id_trg)) 
+			loss = loss + loss_id
             # wandb.log({"iter_loss": loss})
-            print(loss)        
-            loss.backward()
-            optimizer.step()
-            with warmup_scheduler.dampening(): 
-                lr_scheduler.step()
-            
+			print(loss)        
+			loss.backward()
+			optimizer.step()
+			with warmup_scheduler.dampening(): 
+				lr_scheduler.step()
 
-            epoch_loss +=loss
 
-        epoch_loss = epoch_loss.div_(len(train_loader))
+			epoch_loss +=loss
+
+		epoch_loss = epoch_loss.div_(len(train_loader))
         # wandb.log({"epoch_loss": epoch_loss})
 
         # wandb.log({"pred_vid": wandb.Video(pred[1,:,:,:].unsqueeze(1).repeat(1,3,1,1).detach().numpy(), fps=2)})
         # wandb.log({"trg_vid": wandb.Video(trg[1,:,:,:].unsqueeze(1).repeat(1,3,1,1).detach().numpy(), fps=2)})
-        save_frames(batch=pred, example=1, window_size=pred.shape[1], name=f"pred_epoch_{epoch}")
-        save_frames(batch=trg, example=1, window_size=trg.shape[1], name=f"trg_epoch_{epoch}")
+		save_frames(batch=pred, example=1, window_size=pred.shape[1], name=f"pred_epoch_{epoch}")
+		save_frames(batch=trg, example=1, window_size=trg.shape[1], name=f"trg_epoch_{epoch}")
 
     # wandb.finish()
 
+if __name__ == "__main__": 
+	main()
